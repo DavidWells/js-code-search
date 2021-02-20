ac173f74407bd67b4b6bc5a61e82743e5cdd6cd2
DavidWells <>
2021-02-19 22:22:17 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [ word, setWord ] = useState('')
+  const codeSandbox = `https://codesandbox.io/search?refinementList%5Btemplate%5D=&refinementList%5Bnpm_dependencies.dependency%5D%5B0%5D=${word}&refinementList%5Btags%5D=&page=1&configure%5BhitsPerPage%5D=12`
+  const githubPkg = `https://github.com/search?o=desc&q=filename%3Apackage.json+%22${word}%22&s=indexed&type=Code`
+  const githubJScode = `https://github.com/search?l=JavaScript&o=desc&s=indexed&type=Code&q=require(%22${word}%22)%20OR%20from%20%22${word}%22`
+  const sourceGraph = `https://sourcegraph.com/search?q=%22${word}%22+lang:javascript++&patternType=literal`
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>JS Code Search</h1>
+      <input 
+        onChange={(e) => setWord(e.target.value)}
+        value={word}
+      />
+      <p>Searching for {word}</p>
+      <p>
+        <a href={codeSandbox} target='_blank' rel="noopener">
+          On code sandbox
         </a>
-      </header>
+      </p>
+      <p>
+        <a href={githubPkg} target='_blank' rel="noopener">
+          On Github in package.json
+        </a>
+      </p>
+      <p>
+        <a href={githubJScode} target='_blank' rel="noopener">
+          On Github via require/import statement
+        </a>
+      </p>
+      <p>
+        <a href={sourceGraph} target='_blank' rel="noopener">
+          On sourceGraph
+        </a>
+      </p>
     </div>
-  );
+  )
 }
 
 export default App;

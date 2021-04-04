aef59a08d1ea5086970484ac337dfd3cdc9b8c45
David Wells <>
2021-04-04 14:32:14 -0700

    add google

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import './App.css';
 
 function App() {
+  const inputRef = useRef()
   const [ word, setWord ] = useState('')
+
+
+  useEffect(() => {
+    if (inputRef?.current) {
+      setTimeout(() => {
+        inputRef.current.focus()
+      }, 500)
+    }
+  }, [])
+
+
   const cleanWord = formatWord(word)
   const codeSandbox = `https://codesandbox.io/search?refinementList%5Btemplate%5D=&refinementList%5Bnpm_dependencies.dependency%5D%5B0%5D=${word}&refinementList%5Btags%5D=&page=1&configure%5BhitsPerPage%5D=12`
   const githubPkg = `https://github.com/search?o=desc&q=filename%3Apackage.json+${cleanWord}&s=indexed&type=Code`
@@ -11,46 +23,65 @@ function App() {
 
   const sourceGraphRequire = `https://sourcegraph.com/search?q=require%5C%28%28%27%7C"%29${word}%28%27%7C"%29%5C%29+%28lang:javascript+OR+lang:typescript%29&patternType=regexp`
   const sourceGraphImport = `https://sourcegraph.com/search?q=%28import.*%28%27%7C"%29${word}%28%27%7C"%29%29%28lang:javascript+OR+lang:typescript%29&patternType=regexp`
+
+  let linksRender
+  if(word) {
+    linksRender = (
+      <div>
+        <p>Searching for <strong>{word}</strong></p>
+        <p>
+          <a href={codeSandbox} target='_blank' rel="noopener">
+            Code sandbox examples of {word}
+          </a>
+        </p>
+    
+        <p>
+          <a href={githubPkg} target='_blank' rel="noopener">
+            Github projects package.json using {word}
+          </a>
+        </p>
+        <p>
+          <a href={githubJSImport} target='_blank' rel="noopener">
+            Github projects via <strong>import {word}</strong>
+          </a>
+        </p>
+        <p>
+          <a href={githubJScode} target='_blank' rel="noopener">
+            Github projects via <strong>require("{word}") </strong>
+          </a>
+        </p>
+        <p>
+          <a href={sourceGraphRequire} target='_blank' rel="noopener">
+            SourceGraph <strong>require("{word}") </strong>
+          </a>
+        </p>
+        <p>
+          <a href={sourceGraphImport} target='_blank' rel="noopener">
+            SourceGraph <strong>import {word}</strong>
+          </a>
+        </p>
+        <p>
+          <a href={`https://www.google.com/search?q=${word}+-npmjs.com+-amazon.com+-amazonaws.cn&biw=2844&bih=1387&tbs=qdr:y`} target='_blank' rel="noopener">
+            Google results for <strong>{word}</strong> past year
+          </a>
+          
+        </p>
+      </div>
+    )
+  }
+
   return (
     <div className="App">
       <h1>JS Code Search</h1>
       <p>Search for usage examples of any npm package!</p>
       <input 
+        ref={inputRef}
         onChange={(e) => setWord(e.target.value)}
         value={word}
+        style={{padding: '8px 12px', width: 250, fontSize: 16}}
+        placeholder='Enter name of package here'
       />
-      <p>Searching for <strong>{word}</strong></p>
-      <p>
-        <a href={codeSandbox} target='_blank' rel="noopener">
-          Code sandbox examples of {word}
-        </a>
-      </p>
-   
-      <p>
-        <a href={githubPkg} target='_blank' rel="noopener">
-          Github projects package.json using {word}
-        </a>
-      </p>
-      <p>
-        <a href={githubJSImport} target='_blank' rel="noopener">
-           Github projects via <strong>import {word}</strong>
-        </a>
-      </p>
-      <p>
-        <a href={githubJScode} target='_blank' rel="noopener">
-           Github projects via <strong>require("{word}") </strong>
-        </a>
-      </p>
-      <p>
-        <a href={sourceGraphRequire} target='_blank' rel="noopener">
-          SourceGraph <strong>require("{word}") </strong>
-        </a>
-      </p>
-      <p>
-        <a href={sourceGraphImport} target='_blank' rel="noopener">
-          SourceGraph <strong>import {word}</strong>
-        </a>
-      </p>
+      {linksRender}
     </div>
   )
 }
